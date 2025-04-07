b39d1d7374ccb58dde8909e0291a9553a1c78a5e
chiscungg0411 <ccuong0411@gmail.com>
2025-04-08 04:12:47 +0700

    update

--- bot.js
+++ bot.js
@@ -11,30 +11,34 @@ app.use(express.json());
 // Lệnh /start
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Xin chào! Mình là trợ lý UTH. Dùng /lichhoc để xem lịch học nhé!");
+  bot.sendMessage(
+    chatId, "👋 Xin chào! Mình là Trợ lý UTH.\n" +
+            "📅 /tuannay - Lấy lịch học tuần này.\n");
 });
 
 // Lệnh /lichhoc
 bot.onText(/\/lichhoc/, async (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "📅 Đang lấy lịch học, chờ chút nhé...");
+  bot.sendMessage(chatId, "📅 Đang lấy lịch học tuần này, vui lòng chờ trong giây lát ⌛...");
 
   try {
     const { schedule, week } = await getSchedule();
-    let reply = `📅 **Lịch học tuần từ ngày ${week}**\n`;
-    reply += "Xem chi tiết tại: [https://portal.ut.edu.vn/calendar](https://portal.ut.edu.vn/calendar)\n\n";
+    let reply = `📅 **Lịch học tuần này của bạn:**\n`;
+    reply += "ℹ️ Hãy truy cập [Portal UTH](https://portal.ut.edu.vn/dashboard) để biết thêm thông tin chi tiết.\n\n";
 
     if (Object.keys(schedule).length) {
       for (const [day, classes] of Object.entries(schedule)) {
         if (classes.length > 0) {
           reply += `**${day}**:\n`;
           classes.forEach((item, index) => {
-            reply += `${index + 1}. ${item.shift}: ${item.subject}\n   - Giờ: ${item.time}\n   - Phòng: ${item.room}\n`;
+            reply += `${index + 1}. ${item.shift}: ${item.subject}\n   
+            - 🕛 Giờ bắt đầu: ${item.time}\n   
+            - 📍 Phòng học: ${item.room}\n`;
           });
           reply += "\n";
         }
       }
-      if (reply === `📅 **Lịch học tuần từ ngày ${week}**\nXem chi tiết tại: [https://portal.ut.edu.vn/calendar](https://portal.ut.edu.vn/calendar)\n\n`) {
+      if (reply === `📅 **Lịch học tuần này của bạn:**\nℹ️ Hãy truy cập [Portal UTH](https://portal.ut.edu.vn/dashboard) để biết thêm thông tin chi tiết.\n\n`) {
         reply += "❌ Không có lịch học trong tuần này.";
       }
     } else {
