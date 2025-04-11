a780c76b350f1d1ee867591b098bf46d08eff685
Võ Chí Cường <166814228+chiscungg0411@users.noreply.github.com>
2025-04-12 01:14:03 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-// bot.js
 require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
@@ -138,13 +137,13 @@ bot.onText(/\/congno/, async (msg) => {
   bot.sendMessage(chatId, "💰 Đang lấy thông tin công nợ, vui lòng chờ trong giây lát... ⌛");
 
   try {
-    const { totalCredits, totalTuition, totalDebt } = await getTuition(launchBrowser);
+    const tuition = await getTuition(launchBrowser);
     const message =
       `💰 **Thông tin công nợ của bạn:**\n` +
       `------------------------------------\n` +
-      `📚 **Tổng tín chỉ:** ${totalCredits}\n` +
-      `💸 **Tổng học phí:** ${totalTuition}\n` +
-      `📉 **Công nợ:** ${totalDebt}\n` +
+      `📚 **Tổng tín chỉ:** ${tuition.totalCredits}\n` +
+      `💸 **Tổng học phí:** ${tuition.totalTuition}\n` +
+      `📉 **Công nợ:** ${tuition.totalDebt}\n` +
       `------------------------------------\n` +
       `✅ Dữ liệu được lấy từ tab "Học phí ngành" với tùy chọn "Tất cả".`;
 
